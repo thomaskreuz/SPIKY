d4cf005faa7079ccdc8172ea6d94e2addfd62672
N3bojsa <nebojsa.bozanic@fi.isc.cnr.it>
2014-04-29 18:58:54 +0200

    Update poisspdf.m

--- poisspdf.m
+++ poisspdf.m
@@ -1,46 +1,38 @@
-function x = poissinv(p,lambda)
-%POISSINV Inverse of the Poisson cumulative distribution function (cdf).
-%	X = POISSINV(P,LAMBDA) returns the inverse of the Poisson cdf 
-%	with parameter lambda. Since the Poisson distribution is discrete,
-%	POISSINV returns the smallest value of X, such that the poisson 
-%	cdf evaluated, at X, equals or exceeds P.
+function y = poisspdf(x,lambda)
+%POISSPDF Poisson probability density function.
+%	Y = POISSPDF(X,LAMBDA) returns the Poisson probability density 
+%	function with parameter LAMBDA at the values in X.
 %
-%	The size of X is the common size of P and LAMBDA. A scalar input   
+%	The size of Y is the common size of X and LAMBDA. A scalar input   
 %	functions as a constant matrix of the same size as the other input.	 
+%
+%	Note that the density function is zero unless X is an integer.
 
-%	B.A. Jones 1-15-93
+%	References:
+%	   [1]  M. Abramowitz and I. A. Stegun, "Handbook of Mathematical
+%	   Functions", Government Printing Office, 1964, 26.1.22.
+
 %	Copyright (c) 1993 by The MathWorks, Inc.
-%	$Revision: 1.1 $  $Date: 1993/05/24 18:55:53 $
+%	$Revision: 1.1 $  $Date: 1993/05/24 18:55:56 $
 
-if nargin < 2, 
+if nargin <  2, 
     error('Requires two input arguments.'); 
 end
 
-[errorcode p lambda] = distchck(2,p,lambda);
+[errorcode x lambda] = distchck(2,x,lambda);
 
 if errorcode > 0
     error('The arguments must be the same size or be scalars.');
 end
 
-x = zeros(size(p));
+y = zeros(size(x));
 
-cumdist = poisspdf(0,lambda);
-count = 0;
+k1 = find(lambda <= 0);
+if any(k1) 
+    y(k1) = NaN * ones(size(k1));
+end 
 
-% Compare P to the poisson cdf.
-k = find(lambda > 0 & p >= 0 & p <= 1);
-while any(any(p(k) > cumdist(k)))
-    count = count + 1;
-    cumdist(k) = cumdist(k) + poisspdf(count,lambda(k));
-    if count == 1
-            x(k) = x(k) + 1;
-    end
-    index = find(cumdist(k) < p(k));
-    x(k(index)) = x(k(index)) + 1;  
-end
-
-% Return NaN if the arguments are outside their respective limits.
-k = find(lambda <= 0 | p < 0 | p > 1);
+k = find(x >= 0 & x == round(x) & lambda > 0);
 if any(k)
-    x(k) = NaN * ones(size(k));
+    y(k) = exp(-lambda(k) + x(k) .* log(lambda(k)) - gammaln(x(k) + 1));
 end
